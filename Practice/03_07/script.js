5a0dab3467721d8f81402f2401e2fbf3fdb6c352
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-08-16 13:41:38 -0400

    completed practice 1

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,45 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const guitar = {
+  name: "oddly",
+  guitarManufacturer: "Ibanez",
+  stringsNum: 4,
+  neckLength: 32,
+  pickups: 2,
+  weight: 12,
+  hasStrap: true,
+  toggleStrap: function (value) {
+    this.hasStrap = value;
+  },
+};
+
+console.log(guitar);
+console.log(guitar.hasStrap);
+
+guitar.toggleStrap(false);
+console.log(guitar);
+
+const sunglasses = {
+  name: "skullerz",
+  age: 1,
+  hasScratches: "true",
+  toggleScratches: function (value) {
+    this.hasScratches = true;
+  },
+  lost: false,
+  lose: function () {
+    console.log("Shit! I lost my", sunglasses.name);
+    this.lost = true;
+  },
+  find: function () {
+    console.log(
+      "Oooooh yea!!! Now Were Talking! Thats where I left my",
+      sunglasses.name
+    );
+    this.lost = false;
+  },
+};
+
+console.log(sunglasses);
