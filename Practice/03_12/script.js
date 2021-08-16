2ad45af8df60d4bf2a319445eb5c4e0dfac7d7b1
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-08-16 14:38:28 -0400

    completed practice 3

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,58 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+class Shirt {
+  constructor(name, color, size, threadbare = false, wearing) {
+    //properties
+    this.name = name;
+    this.color = color;
+    this.size = size;
+    this.threadbare = threadbare;
+    this.wearing = wearing;
+  }
+  //methods
+  toggleThreadbare() {
+    this.threadbare = !this.threadbare;
+  }
+  wear() {
+    this.wearing = true;
+  }
+  takeOff() {
+    this.wearing = false;
+  }
+}
+
+const blueStriped = new Shirt("striped", "blue", "large", false, false);
+
+console.log(blueStriped);
+
+blueStriped.wear();
+
+console.log(blueStriped);
+
+blueStriped.toggleThreadbare();
+
+blueStriped.takeOff();
+
+console.log(blueStriped);
+
+export default Shirt;
+
+//object constructor function
+// function Pants(name, color, size, threadbare = false) {
+//   this.name = name;
+//   this.color = color;
+//   this.size = size;
+//   this.threadbare = threadbare;
+//   this.toggleThreadbare = function () {
+//     this.threadbare = !this.threadbare;
+//   };
+// }
+
+// const denim = new Pants("jeans", "blue", 40, false);
+
+// console.log(denim);
+// denim.toggleThreadbare();
+// console.log(denim);
+// export default Pants
